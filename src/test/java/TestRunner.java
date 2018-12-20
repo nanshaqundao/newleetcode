f54f08edc11384fd7728704e54ee6a75f9aa78d4
Nan Sha <iamshanan@gmail.com>
2018-12-20 23:20:00 +0000

    setup correct test runner

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,15 +1,11 @@
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
-import stepDefinition.Problem008Step;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/resources"},
+        format = {"pretty", "html:target/Destination"})
 public class TestRunner {
-    public static void main(String[] args) {
-        Result result = JUnitCore.runClasses(Problem008Step.class);
-        for(Failure failure: result.getFailures()){
-            System.out.println(failure.toString());
-        }
 
-        System.out.println("Result == " + result.wasSuccessful());
-    }
 }
