62828190c7dc15d8ad6eea6a345356b90cc0e5e4
iamrukia <iamshanan@gmail.com>
2020-01-07 19:14:56 +0000

    no 11 - max area containing water - two pointer solution

--- src/main/java/array/medium/p0011/MostWater.java
+++ src/main/java/array/medium/p0011/MostWater.java
@@ -1,7 +1,8 @@
 package array.medium.p0011;
 
 public class MostWater {
-    public int maxArea(int[] height) {
+    //brutal force
+    public int maxArea_brutal_force(int[] height) {
         int tempMax = 0;
         for (int i = 0; i < height.length - 1; i++) {
             for (int j = i + 1; j < height.length; j++) {
@@ -10,5 +11,27 @@ public class MostWater {
             }
         }
         return tempMax;
+    }
+
+    // two pointer
+    public int maxArea(int[] height) {
+        int tempMax = 0;
+        int lastMax = 0;
+        int i = 0;
+        int j = height.length - 1;
+        for (int index = 0; index < height.length; index++) {
+            if (height[i] > height[j]) {
+                tempMax = (j - i) * height[j];
+                j = j - 1;
+            } else {
+                tempMax = (j - i) * height[i];
+                i = i + 1;
+            }
+            lastMax = Math.max(tempMax, lastMax);
+            if (i == j) {
+                break;
+            }
+        }
+        return lastMax;
     }
 }
