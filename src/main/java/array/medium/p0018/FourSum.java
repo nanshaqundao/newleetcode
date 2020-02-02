380a41b8afa3dba11a3b50bd45afb2daa7f35584
iamrukia <iamshanan@gmail.com>
2020-02-02 20:35:09 +0000

    new solution using 2 pointers now make the processing tiem down from 68ms to 16ms still i should look at some other magicall solution which only takes 3 ms.

--- src/main/java/array/medium/p0018/FourSum.java
+++ src/main/java/array/medium/p0018/FourSum.java
@@ -34,4 +34,47 @@ public class FourSum {
 
         return new ArrayList<>(results);
     }
+
+    // 2nd solution, quicker
+    public List<List<Integer>> fourSumNew(int[] nums, int target) {
+        // sort the array first
+        Arrays.sort(nums);
+
+        Set<List<Integer>> results = new HashSet<>();
+        int length = nums.length;
+        // if the initial array has less than 4 entries, there is no valid solution -> thus return null
+        if (length < 4) {
+            return new ArrayList<>();
+        }
+
+        int a;
+        int b;
+        int c;
+        int d;
+        for (int i = 0; i < length - 3; i++) {
+            for (int j = i + 1; j < length - 2; j++) {
+                a = nums[i];
+                b = nums[j];
+                int k = j + 1;
+                int l = length - 1;
+                // double pointer
+                while (k < l) {
+                    c = nums[k];
+                    d = nums[l];
+                    int sum = a + b + c + d;
+                    if (sum == target) {
+                        results.add(Arrays.asList(a, b, c, d));
+                        k++;
+                        l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
+                }
+
+            }
+        }
+        return new ArrayList<>(results);
+    }
 }
